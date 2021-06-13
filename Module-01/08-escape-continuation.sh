5882f67f2aef2c0447f0cbf3bdd97b024b8f6d71
Charles Ross <chivalry@mac.com>
2021-06-13 14:29:57 -0700

    escape continuation

---
+++ Module-01/08-escape-continuation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "The title of the book is \"Linux\""
+echo "The cost of the book is \$12.50"
+echo "This is a sample line \
+that is very long ..........\
+............................\
+............................\
+.....................It is \
+really long."
