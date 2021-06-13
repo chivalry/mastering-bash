f60722a9e90ca82da788706710bd7cfa320b76e0
Charles Ross <chivalry@mac.com>
2021-06-13 16:18:21 -0700

    environment variables

---
+++ Module-02/04.env-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Welcome to GlobaleTraining.com"
+echo "1: My home dir is - ${HOME}"
+echo "2: My login id is - ${LOGNAME}"
+echo "3: My shell is - ${SHELL}"
+echo "4: My course is - ${COURSE}"
