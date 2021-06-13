83e3e95323bbfffad3980bf6fbba5d0c4197a032
Charles Ross <chivalry@mac.com>
2021-06-13 16:02:32 -0700

    variables and global variables

---
+++ Module-02/02-variables2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Welcome to GlobaleTraining.com"
+
+course_name="Bash Shell Scripting"
+echo "1: Welcome to ${course_name}"
---
+++ Module-02/03.globalvariables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Welcome to GlobaleTraining.com"
+
+export course_name3="AWS VPC Mastery Course"
+echo "1: Weclome to ${course_name}"
+echo "2: Welcome to ${course_name2}"
+echo "3: Welcome to ${course_name3}"
