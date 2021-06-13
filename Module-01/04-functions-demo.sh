a853acf4a8e1dd99bf4996be2f3ef0668301ea5a
Charles Ross <chivalry@mac.com>
2021-06-13 13:16:51 -0700

    function demo

---
+++ Module-01/04-functions-demo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "1: Welcome to Bash Scripting course!"
+
+function welcome_message2 {
+    echo "2: Welcome to Bash Scripting course!"
+}
+
+welcome_message3() {
+    echo "3: Welcome to Bash Scripting course!"
+}
+
+welcome_message2
+welcome_message3
