e0c50188ed49c830173862a2b2d461f498156536
Charles Ross <chivalry@mac.com>
2021-06-13 16:27:01 -0700

    positional parameters

---
+++ Module-02/05-shell-vars-positional-parameters.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Welcome to GlobaleTraining.com"
+
+echo "The 1st Argument : $1"
+echo "The 2nd Argument : $2"
+echo "The 3rd Argument : $3"
+echo "The 4th Argument : $4"
+echo "The 5th Argument : $5"
+
+echo "Total Args: $#"
+
+echo "script name: $0"
