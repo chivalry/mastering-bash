f03e0db680b1afeb3182d114a8bbbe380ea103fc
Charles Ross <chivalry@mac.com>
2021-06-13 17:11:22 -0700

    arithmetic operators 2

---
+++ Module-03/03-arithmetic-operators-2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Welcome to GlobaleTraining.com"
+
+a=10
+b=3
+
+sum=$(( a + b))
+echo "Sum: ${sum}"
+
+sub=$(( a - b ))
+echo "Subtract: ${sub}"
+
+multi=$(( a * b ))
+echo "Multiply: ${multi}"
+
+div=$(( a / b ))
+echo "Division: ${div}"
+
+mod=$(( a % b ))
+echo "Modulus: ${mod}"
